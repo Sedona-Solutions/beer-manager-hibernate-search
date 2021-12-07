36041c1aca06f524fda408ac6ffc9bbb28fd871a
Edouard lamotte <elamotte@sedona.fr>
2021-12-07 12:15:50 +0100

    remove hibernate-search from initial branch

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,9 @@
 INSERT INTO Brewery(id, name, type) VALUES (1, 'Kronenbourg', 'INDUSTRIAL');
 INSERT INTO Brewery(id, name, type) VALUES (2, 'La Bière de la Rade', 'CRAFT');
+
 INSERT INTO Beer(id, name, brewery_id, alcoholLevel) VALUES (1, '1664', 1, 5.5);
 INSERT INTO Beer(id, name, brewery_id, alcoholLevel) VALUES (2, 'Censurée', 2, 5.8);
+
 INSERT INTO Beer_Flavors(Beer_id, flavors) VALUES (1, 'Houblon floral');
 INSERT INTO Beer_Flavors(Beer_id, flavors) VALUES (1, 'Malt');
 INSERT INTO Beer_Flavors(Beer_id, flavors) VALUES (2, 'Houblon');
