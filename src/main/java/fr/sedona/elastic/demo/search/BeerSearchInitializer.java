521a86debba73b9b62f73e4f5cddb94bc5ff49de
Edouard lamotte <elamotte@sedona.fr>
2021-12-07 11:56:57 +0100

    init hibernate-search

---
+++ src/main/java/fr/sedona/elastic/demo/search/BeerSearchInitializer.java
@@ -0,0 +1,30 @@
+package fr.sedona.elastic.demo.search;
+
+import io.quarkus.runtime.StartupEvent;
+import org.hibernate.search.mapper.orm.session.SearchSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
+/**
+ * Initializer used to reindex all entities on application startup
+ */
+public class BeerSearchInitializer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BeerSearchInitializer.class);
+
+    private SearchSession searchSession;
+
+    @Inject
+    public BeerSearchInitializer(SearchSession searchSession) {
+        this.searchSession = searchSession;
+    }
+
+    public void reindexOnStartup(@Observes StartupEvent startupEvent) throws InterruptedException {
+        this.searchSession.massIndexer().startAndWait();
+        LOGGER.info("Data in DB reindexed");
+    }
+
+}
