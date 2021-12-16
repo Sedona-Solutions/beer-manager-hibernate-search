6546d23024e6af58699a2061ac01df44c404d49f
Thomas Cucchietti <tcucchietti@sedona.fr>
2021-12-16 10:04:46 +0100

    Add external user entity

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,12 @@
+INSERT INTO ExternalUser(id,lastName,firstName) VALUES (1, 'Lamotte', 'Edouard');
+INSERT INTO ExternalUser(id,lastName,firstName) VALUES (2, 'Cucchietti', 'Thomas');
+INSERT INTO ExternalUser(id,lastName,firstName) VALUES (3, 'Pilato', 'David');
+
 INSERT INTO Brewery(id, name, type) VALUES (1, 'Kronenbourg', 'INDUSTRIAL');
 INSERT INTO Brewery(id, name, type) VALUES (2, 'La Bière de la Rade', 'CRAFT');
 
-INSERT INTO Beer(id, name, brewery_id, alcoholLevel) VALUES (1, '1664', 1, 5.5);
-INSERT INTO Beer(id, name, brewery_id, alcoholLevel) VALUES (2, 'Censurée', 2, 5.8);
+INSERT INTO Beer(id, name, brewery_id, alcoholLevel, creatorId) VALUES (1, '1664', 1, 5.5, 1);
+INSERT INTO Beer(id, name, brewery_id, alcoholLevel, creatorId) VALUES (2, 'Censurée', 2, 5.8, 2);
 
 INSERT INTO Beer_Flavors(Beer_id, flavors) VALUES (1, 'Houblon floral');
 INSERT INTO Beer_Flavors(Beer_id, flavors) VALUES (1, 'Malt');
