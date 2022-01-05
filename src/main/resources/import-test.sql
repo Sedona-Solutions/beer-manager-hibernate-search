5ee8085aa12a2c71959132355675c0c3c2e494e1
Edouard lamotte <elamotte@sedona.fr>
2022-01-05 12:15:03 +0100

    replace INSERT by REPLACE in *.sql

--- src/main/resources/import-test.sql
+++ src/main/resources/import-test.sql
@@ -1,15 +1,15 @@
-INSERT INTO ExternalUser(id,lastName,firstName) VALUES (1, 'Lamotte', 'Edouard');
-INSERT INTO ExternalUser(id,lastName,firstName) VALUES (2, 'Cucchietti', 'Thomas');
-INSERT INTO ExternalUser(id,lastName,firstName) VALUES (3, 'Pilato', 'David');
+REPLACE INTO ExternalUser(id,lastName,firstName) VALUES (1, 'Lamotte', 'Edouard');
+REPLACE INTO ExternalUser(id,lastName,firstName) VALUES (2, 'Cucchietti', 'Thomas');
+REPLACE INTO ExternalUser(id,lastName,firstName) VALUES (3, 'Pilato', 'David');
 
-INSERT INTO Brewery(id, name, type) VALUES (1, 'Kronenbourg', 'INDUSTRIAL');
-INSERT INTO Brewery(id, name, type) VALUES (2, 'La Bière de la Rade', 'CRAFT');
+REPLACE INTO Brewery(id, name, type) VALUES (1, 'Kronenbourg', 'INDUSTRIAL');
+REPLACE INTO Brewery(id, name, type) VALUES (2, 'La Bière de la Rade', 'CRAFT');
 
-INSERT INTO Beer(id, name, brewery_id, alcoholLevel, creatorId) VALUES (1, '1664', 1, 5.5, 1);
-INSERT INTO Beer(id, name, brewery_id, alcoholLevel, creatorId) VALUES (2, 'Censurée', 2, 5.8, 2);
+REPLACE INTO Beer(id, name, brewery_id, alcoholLevel, creatorId) VALUES (1, '1664', 1, 5.5, 1);
+REPLACE INTO Beer(id, name, brewery_id, alcoholLevel, creatorId) VALUES (2, 'Censurée', 2, 5.8, 2);
 
-INSERT INTO Beer_Flavors(Beer_id, flavors) VALUES (1, 'Houblon floral');
-INSERT INTO Beer_Flavors(Beer_id, flavors) VALUES (1, 'Malt');
-INSERT INTO Beer_Flavors(Beer_id, flavors) VALUES (2, 'Houblon');
-INSERT INTO Beer_Flavors(Beer_id, flavors) VALUES (2, 'Mangue');
-INSERT INTO Beer_Flavors(Beer_id, flavors) VALUES (2, 'Fruits exotiques');
+REPLACE INTO Beer_Flavors(Beer_id, flavors) VALUES (1, 'Houblon floral');
+REPLACE INTO Beer_Flavors(Beer_id, flavors) VALUES (1, 'Malt');
+REPLACE INTO Beer_Flavors(Beer_id, flavors) VALUES (2, 'Houblon');
+REPLACE INTO Beer_Flavors(Beer_id, flavors) VALUES (2, 'Mangue');
+REPLACE INTO Beer_Flavors(Beer_id, flavors) VALUES (2, 'Fruits exotiques');
